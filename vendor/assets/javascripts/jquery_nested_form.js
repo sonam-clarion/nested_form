b495fc7d1954c39459d0831018461835132b9e85
sonam-clarion <sonam.shah@clariontechnologies.co.in>
2014-03-26 17:55:05 +0530

    Update jquery_nested_form.js

--- vendor/assets/javascripts/jquery_nested_form.js
+++ vendor/assets/javascripts/jquery_nested_form.js
@@ -12,36 +12,6 @@
       var blueprint = $('#' + $(link).data('blueprint-id'));
       var content   = blueprint.data('blueprint');                // Fields template
 
-      // Make the context correct by replacing <parents> with the generated ID
-      // of each of the parent objects
-      var context = ($(link).closest('.fields').closestChild('input, textarea, select').eq(0).attr('name') || '').replace(/\[[a-z_]+\]$/, '');
-
-      // If the parent has no inputs we need to strip off the last pair
-      var current = content.match(new RegExp('\\[([a-z_]+)\\]\\[new_' + assoc + '\\]'));
-      if (current) {
-        context = context.replace(new RegExp('\\[' + current[1] + '\\]\\[(new_)?\\d+\\]$'), '');
-      }
-
-      // context will be something like this for a brand new form:
-      // project[tasks_attributes][1255929127459][assignments_attributes][1255929128105]
-      // or for an edit form:
-      // project[tasks_attributes][0][assignments_attributes][1]
-      if (context) {
-        var parentNames = context.match(/[a-z_]+_attributes(?=\]\[(new_)?\d+\])/g) || [];
-        var parentIds   = context.match(/[0-9]+/g) || [];
-
-        for(var i = 0; i < parentNames.length; i++) {
-          if(parentIds[i]) {
-            content = content.replace(
-              new RegExp('(_' + parentNames[i] + ')_.+?_', 'g'),
-              '$1_' + parentIds[i] + '_');
-
-            content = content.replace(
-              new RegExp('(\\[' + parentNames[i] + '\\])\\[.+?\\]', 'g'),
-              '$1[' + parentIds[i] + ']');
-          }
-        }
-      }
 
       // Make a unique ID for the new child
       var regexp  = new RegExp('new_' + assoc, 'g');
